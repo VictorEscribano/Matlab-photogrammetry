fd5c8b75ae2d3273da87d1c2bc347c4cda5a8492
VictorEscribano <70441479+VictorEscribano@users.noreply.github.com>
2022-12-09 13:23:52 +0100

    disparity_map fix

--- Scripts/snapshot.m
+++ Scripts/snapshot.m
@@ -3,8 +3,8 @@ close all;
 clc;
 
 webcamlist
-left=webcam();
-right=webcam(2);
+left=webcam(2);
+right=webcam();
 
 img=snapshot(left);
 img2=snapshot(right);
@@ -13,12 +13,12 @@ preview(left);
 preview(right);
 prompt= 'Take photo?(1/0)';
 out=input(prompt);
-num = 1;
+num = 3;
 while out==1
     left_im = snapshot(left);
     right_im = snapshot(right);
-    left_name = 'Images/Left/left_' + string(num)+'.png';
-    right_name = 'Images/Right/right_' + string(num)+'.png';
+    left_name = 'images/Left/left_' + string(num)+'.png';
+    right_name = 'images/Right/right_' + string(num)+'.png';
     disp('Saving images...')
     imwrite(left_im, left_name);
     imwrite(right_im, right_name);
